d80a5858ff41f2dd95189157c0726e6dff0e48e6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-12-04 16:30:49 +0330

    full removed contextapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { useRoutes } from "react-router-dom"
 import routes from "./routes";
-import WeatherContext from "./Context/weatherContext";
 
 function App() {
   const router = useRoutes(routes)
--- src/Context/weatherContext.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-const weatherContext = createContext(null);
-
-export default weatherContext;
